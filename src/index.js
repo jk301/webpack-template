7f8ca1f8f31980588320a44a7fe5cc65c35082d4
junaid <jk7948677@gmail.com>
2026-04-07 12:12:01 +0530

    Changed / Deleted files for template

--- src/index.js
+++ src/index.js
@@ -1,11 +1,1 @@
-// index.js
-import { greeting } from "./greet.js";
-import "./style.css";
-import odinImage from "./img/odin-lined.png";
-
-const img = document.createElement("img");
-img.src = odinImage;
-
-document.body.appendChild(img);
-
-console.log(greeting);
+console.log("helo");
